242defb716fd37d0cb761a485f6e2d69a0009af7
zwet3-10 <zv.pks12@yandex.ru>
2020-06-14 22:34:55 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-
+<!-- index.html -->
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>СоЦветия</title>
+    <style>
+      header {
+        position: absolute;
+        width: 98%;
+        background: #c8d6e6;
+      }
+      
+      header h1 {
+        color: #ffffff;
+        margin: 1%;
+        
+      }
+    </style>
+  </head>
+  <body>
+    <html>
+      <header>
+        <h1>СоЦветия</h1>
+       </header>
+    </html>
+  </body>
+</html>
